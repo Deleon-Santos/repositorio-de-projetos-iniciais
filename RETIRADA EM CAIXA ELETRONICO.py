6318365004ef10f3299af8089ad92e934a7d29c8
Deleon santos <delps.santos1987@gmail.com>
2023-10-06 12:10:34 -0300

    'otimizando funcoes'

--- RETIRADA EM CAIXA ELETRONICO.py
+++ RETIRADA EM CAIXA ELETRONICO.py
@@ -1,3 +1,5 @@
+from ast import Continue
+
 # CONSTRUA UM SISTEMA DE RETIRADA DE VALORES SEMELHANTE AO CAIXA ELETRONICO,
 # FORNEÇA MECANISMOS PARA VALIDADAR LOGIN E SENHAS
 # VERIFIQUE SE HA SALDO DISPONIVEL EM CADA RETIRADA
@@ -50,63 +52,65 @@ def saque_conta(saldo):
             continue
 
 
+def total(titular, saldo):
+    while True:
+        print(">>" * 20)
+        print(f"\n***Ola, {titular}! Bem - Vindo a sua conta!***")
+        menu = input(f"\nEscolha uma operação:\n" +
+                     "1-saldo\n" +
+                     "2-saque\n" +
+                     "0-sai\n" +
+                     ">>")
+
+        if menu == "1":
+            saldo_l = saldo_conta(saldo)
+            continue
+
+        elif menu == "2":
+            saque_l = saque_conta(saldo)
+            print(saque_l)
+            continue
+
+        elif menu == "0":
+            print(">>" * 20)
+            print(f"\n{titular} Obrigado por ultilizar nossos serviços")
+            t = sum(lista_saque)
+            l = len(lista_saque)
+            t2 = saldo - t
+            print(f"\nVocê realizou o total de {l} saque(s) no valor total R$ {t:.2f}")
+            print(f">>O saldo desta conta é R$ {t2:.2f}")
+            print("\nVeja o historico de saques")
+            print(lista_saque)
+            print()
+            print("***OPERAÇÃO ENCERRADA!***")
+            break
+        else:
+            print("Escolha uma opção do menu")
+            continue
+
+
 # *********************Inicio do Programa Principal*****************************
 while True:
     cartao = input("\nDigite aqui o numero do seu cartão, ou 'S'sair >> ").upper()[0]
     if cartao[0] == "S":
         print("***OPERAÇÃO ENCERRADA!***")
         break
+
     senha1 = input("Digite aqui a sua senha >> ")
     for c in cliente:
-        if c['num'] == cartao and c["senha"] == senha1:
+        if c['num'] != cartao or c["senha"] != senha1:
+            # print('***Usuario ou senha não encontrado!***')
+            continue
+        else:
             saldo = c["saldo"]
             titular = c["nome"].upper()
             senha = c["senha"]
 
-            while True:
-                print(">>" * 20)
-                print(f"\n***Ola, {titular}! Bem - Vindo a sua conta!***")
-                menu = input(f"\nEscolha uma operação:\n" +
-                             "1-saldo\n" +
-                             "2-saque\n" +
-                             "0-sai\n" +
-                             ">>")
-
-                if menu == "1":
-                    saldo_l = saldo_conta(saldo)
-                    continue
-
-                elif menu == "2":
-                    saque_l = saque_conta(saldo)
-                    print(saque_l)
-                    continue
-
-                elif menu == "0":
-                    print(">>" * 20)
-                    print(f"\n{titular} Obrigado por ultilizar nossos serviços")
-                    t = sum(lista_saque)
-                    l = len(lista_saque)
-                    t2 = saldo - t
-                    print(f"\nVocê realizou o total de {l} saque(s) no valor total R$ {t:.2f}")
-                    print(f">>O saldo desta conta é R$ {t2:.2f}")
-                    print("\nVeja o historico de saques")
-                    print(lista_saque)
-                    print()
-                    print("***OPERAÇÃO ENCERRADA!***")
-                    break
-
-                else:
-                    print("Escolha uma opção do menu")
-                    continue
+            menu = total(titular, saldo)
+            print(menu)
             break
-
-        else:
-            print('\n***Usuário ou Senha não encontrado!***')
-            continue
-    break
-
-
-
+    print('***Usuario ou senha não encontrado!***')
+    continue
 
 
 
